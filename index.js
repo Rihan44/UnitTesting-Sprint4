bfcc881bd456bdaaf16e42e0ee60597e385495e5
Rihan44 <asmuela_bustamante@hotmail.es>
2023-10-10 15:34:24 +0200

    test availableRooms

--- index.js
+++ index.js
@@ -1,59 +1,128 @@
 
 class Room {
-    constructor(name, bookings, rate, discount) {
-        this.name = name;
-        this.bookings = bookings;
-        this.rate = rate;
-        this.discount = discount;
+  constructor(name, bookings, rate, discount) {
+    this.name = name;
+    this.bookings = bookings;
+    this.rate = rate;
+    this.discount = discount;
+  }
+
+  isOccupied(date) {
+    const myDate = new Date(date);
+
+    for (let i = 0; i < this.bookings.length; i++) {
+      const startDate = new Date(this.bookings[i].checkIn);
+      const endDate = new Date(this.bookings[i].checkOut);
+
+      if (myDate >= startDate && myDate <= endDate) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  occupancyPercentage(startingDate, endingDate) {
+    const startDate = new Date(startingDate);
+    const endDate = new Date(endingDate);
+
+    startDate.setHours(0, 0, 0, 0);
+    endDate.setHours(23, 59, 59, 999);
+
+    let totalDaysInRange = 0;
+    let occupiedDays = 0;
+
+    for(let currentDate = startDate; currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
+
+      totalDaysInRange++;
+
+      let isOccupied = false;
+
+      for(const booking of this.bookings) {
+        const bookingStartDate = new Date(booking.checkIn);
+        const bookingEndDate = new Date(booking.checkOut);
+        bookingStartDate.setHours(0, 0, 0, 0);
+        bookingEndDate.setHours(23, 59, 59, 999);
+
+        if (currentDate >= bookingStartDate && currentDate <= bookingEndDate) {
+          isOccupied = true;
+          break;
+        }
+      }
+
+      if (isOccupied) {
+        occupiedDays++;
+      }
+    }
+
+    if (totalDaysInRange === 0) {
+      return 0;
+    }
+
+    const percentage = (occupiedDays / totalDaysInRange) * 100;
+
+    return parseFloat(percentage.toFixed(1));
+  }
+
+  static totalOccupancyPercentage(rooms, startDate, endDate) {
+
+    if (!Array.isArray(rooms) || rooms.every((room) => !(room instanceof Room))) {
+      return 0;
+    }
+
+    function countDays(startDate, endDate) {
+      const oneDay = 24 * 60 * 60 * 1000;
+      return Math.round(Math.abs((startDate - endDate) / oneDay)) + 1;
+    }
+
+    let totalOccupiedDays = 0;
+    let totalDaysInRange = countDays(new Date(startDate), new Date(endDate));
+
+    if (totalDaysInRange === 0) {
+      return 0;
+    }
+
+    rooms.forEach((room) => {
+      totalOccupiedDays += room.occupancyPercentage(startDate, endDate);
+    });
+
+    const percentage = (totalOccupiedDays / rooms.length).toFixed(1);
+
+    return parseFloat(percentage);
+  }
+
+  static availableRooms(rooms, startDate, endDate) {
+
+    let availableRooms = [];
+
+    for(let room of rooms) {
+      const ocuppied = room.isOccupied(startDate) || room.isOccupied(endDate);
+      if(!ocuppied) {
+        availableRooms.push(room);
+      }
     }
     
-    isOccupied(date) {
-        for (const booking of this.bookings) {
-            if (booking.date === date)
-                return true;
-        }
-        return false;
-    }
-
-    occupancyPercentage(startDate, endDate) {
-        const oneDay = 24 * 60 * 60 * 1000;
-        const checkStartDate = new Date(startDate).getTime();
-        const checkEndDate = new Date(endDate).getTime();
-        const nighCount = Math.abs(checkEndDate - checkStartDate) / oneDay;
-        const bookingsLength = this.bookings.length;
-        const chequear = (checkEndDate > (new Date(this.bookings[bookingsLength - 1].date)) && checkStartDate < (new Date(this.bookings[0].date)))
-
-        if (chequear) {
-            const roomOccupancy = this.bookings.length
-            const porcentage = (roomOccupancy * 100) / nighCount
-            return Math.ceil(porcentage)
-
-        } else {
-            throw new Error('Error')
-        }
-    }
+    return availableRooms;
+  }
 }
 
-module.exports = Room;
+class Booking {
+  constructor(name, email, checkIn, checkOut, discount, room) {
+    this.name = name;
+    this.email = email;
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+    this.discount = discount;
+    this.room = room;
+  }
 
-/* class Booking {
-    constructor(name, email, check_in, check_out, discount, room) {
-        this.name = name;
-        this.email = email;
-        this.check_in = check_in;
-        this.check_out = check_out;
-        this.discount = discount;
-        this.room = {
-            name,
-            rate,
-            discount
-        }
-    }
+  get fee() {
 
-    get fee() {
-
-    }
+  }
 }
 
-module.exports = Booking; */
+module.exports = {
+  Room,
+  Booking
+};
 
